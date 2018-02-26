d4574adb8edefaa95dd59793f7e405738176ebac
Cedric H. David <cedric.david@jpl.nasa.gov>
2018-02-26 11:50:15 -0800

    New script for Nepal.

---
+++ tst/tst_pub_repr_David_etal_201x_SER.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#*******************************************************************************
+#tst_pub_repr_David_etal_SER.sh
+#*******************************************************************************
+
+#Purpose:
+#This script reproduces all SHBAAM processing steps used in the writing of:
+#David, Cédric H., et al. (201x)
+#xxx
+#DOI: xx.xxxx/xxxxxx
+#The files used are available from:
+#David, Cédric H., et al. (201x)
+#xxx
+#DOI: xx.xxxx/xxxxxx
+#The following are the possible arguments:
+# - No argument: all unit tests are run
+# - One unique unit test number: this test is run
+# - Two unit test numbers: all tests between those (included) are run
+#The script returns the following exit codes
+# - 0  if all experiments are successful 
+# - 22 if some arguments are faulty 
+# - 99 if a comparison failed 
+#Author:
+#Cedric H. David, 2018-2018
+
+
+#*******************************************************************************
+#Publication message
+#*******************************************************************************
+echo "********************"
+echo "Reproducing files for: http://dx.doi.org/xx.xxxx/"
+echo "********************"
+
+
+#*******************************************************************************
+#Select which unit tests to perform based on inputs to this shell script
+#*******************************************************************************
+if [ "$#" = "0" ]; then
+     fst=1
+     lst=99
+     echo "Performing all unit tests: 1-99"
+     echo "********************"
+fi 
+#Perform all unit tests if no options are given 
+
+if [ "$#" = "1" ]; then
+     fst=$1
+     lst=$1
+     echo "Performing one unit test: $1"
+     echo "********************"
+fi 
+#Perform one single unit test if one option is given 
+
+if [ "$#" = "2" ]; then
+     fst=$1
+     lst=$2
+     echo "Performing unit tests: $1-$2"
+     echo "********************"
+fi 
+#Perform all unit tests between first and second option given (both included) 
+
+if [ "$#" -gt "2" ]; then
+     echo "A maximum of two options can be used" 1>&2
+     exit 22
+fi 
+#Exit if more than two options are given 
+
+
+#*******************************************************************************
+#Initialize count for unit tests
+#*******************************************************************************
+unt=0
+
+
+#*******************************************************************************
+#Terrestrial water storage anomalies
+#*******************************************************************************
+unt=$((unt+1))
+if (("$unt" >= "$fst")) && (("$unt" <= "$lst")) ; then
+echo "Running unit test $unt/x"
+run_file=tmp_run_$unt.txt
+cmp_file=tmp_cmp_$unt.txt
+
+echo "- Terrestrial water storage anomalies"
+../src/shbaam_twsa.py                                                          \
+     ../input/GRACE/GRCTellus.JPL.200204_201608.GLO.RL05M_1.MSCNv02CRIv02.nc   \
+     ../input/GRACE/CLM4.SCALE_FACTOR.JPL.MSCNv01CRIv01.nc                     \
+     ../input/SERVIR_STK/Nepal.shp                                             \
+     ../output/SERVIR_STK/timeseries_Nepal_tst.csv                             \
+     ../output/SERVIR_STK/map_Nepal_tst.nc                                     \
+     > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $? ; fi
+
+echo "- Comparing timeseries"
+./tst_cmp_csv.py                                                               \
+     ../output/SERVIR_STK/timeseries_Nepal.csv                                 \
+     ../output/SERVIR_STK/timeseries_Nepal_tst.csv                             \
+     1e-6                                                                      \
+     1e-6                                                                      \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+rm -f $run_file
+rm -f $cmp_file
+echo "Success"
+echo "********************"
+fi
+
+
+#*******************************************************************************
+#Clean up
+#*******************************************************************************
+
+
+#*******************************************************************************
+#End
+#*******************************************************************************
+echo "Passed all tests!!!"
+echo "********************"
